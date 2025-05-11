1f02e772dbfb6fa1f5ff2422097a8c5f7fb3786b
yamao-sys <yamaopanku@gmail.com>
2025-05-11 11:18:47 +0900

    Playwrightの設定

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -4,9 +4,12 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const envFilePath = process.env.CI ? ".env.test" : ".env.test.local";
+dotenv.config({ path: path.resolve(__dirname, envFilePath) });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,10 +29,14 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://127.0.0.1:3100",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    screenshot: {
+      mode: "on",
+      fullPage: true,
+    },
   },
 
   /* Configure projects for major browsers */
@@ -39,15 +46,15 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -71,9 +78,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm dev:test",
+    url: "http://127.0.0.1:3100",
+    reuseExistingServer: !process.env.CI,
+  },
 });
